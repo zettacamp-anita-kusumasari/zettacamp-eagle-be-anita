63e466b8a1aa2fd3b5cd818a65a491dea76b0f51
Anita Kusumasari <anita.kusumasari@zettacamp.pro>
2025-06-09 05:33:44 +0700

    [ZettaCamp][Module 1][Project Setup & CRUD for Student and School Data] 1.3. Setup GraphQL Server with Express.js: Update app.js

--- core/app.js
+++ core/app.js
@@ -1,7 +1,7 @@
 // *************** IMPORT CORE ***************
 const express = require('express');
-const app = express()
-require('dotenv').config()
+const app = express();
+require('dotenv').config();
 const connectDB = require('./database');
 
 // *************** Get the MONGODB_URI value from environment
@@ -13,12 +13,37 @@ const PORT = process.env.PORT || 4000;
 // ****************** Calling the connectDB to connect the app with the MongoDB database
 connectDB(MONGODB_URI);
 
-// ****************** Starting the server on the PORT (4000)
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
+// *************** Import Apollo Server middleware for Express
+const { ApolloServer } = require('apollo-server-express');
 
-// *************** Route that checks whether the server is running properly or not
-app.get('/', (_, res) => {
-    res.send(`app is running on PORT ${PORT}`);
-});
+// *************** Import GraphQL type definitions
+const schoolTypeDefs = require('../modules/School/School.typeDefs.js');
+const studentTypeDefs = require('../modules/Student/Student.typeDefs.js');
+const userTypeDefs = require('../modules/User/User.typeDefs.js');
+
+// *************** Import resolvers for type definitions
+const schoolResolvers = require('../modules/School/School.resolvers.js');
+const studentResolvers = require('../modules/Student/Student.resolvers.js');
+const userResolvers = require('../modules/User/User.resolvers.js');
+
+// *************** Async function to start Apollo Server and integrate it with Express app
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs: [schoolTypeDefs, studentTypeDefs, userTypeDefs],
+    resolvers: [schoolResolvers, studentResolvers, userResolvers],
+  });
+
+  // *************** Start the Apollo server
+  await server.start();
+
+  // *************** Apply Apollo middleware to Express app
+  server.applyMiddleware({ app });
+
+  // *************** Start the Express server
+  app.listen(PORT, () => {
+    console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+}
+
+// *************** Invoke the function to launch server
+startApolloServer();
