86f6def7fc305e2543af39c56fed357bd8c0c40d
Anita Kusumasari <anita.kusumasari@zettacamp.pro>
2025-06-09 00:10:27 +0700

    [ZettaCamp][Module 1][Project Setup & CRUD for Student and School Data] 1.1. Initial Backend Project Environment Setup: Create express application (app.js)

---
+++ app.js
@@ -0,0 +1,17 @@
+// *************** IMPORT CORE ***************
+const express = require('express');
+const app = express()
+require('dotenv').config()
+
+// *************** Setup PORT Value from Environment
+const PORT = process.env.PORT || 4000;
+
+// ****************** Starting the server on the PORT (4000)
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+// *************** Route that checks whether the server is running properly or not
+app.get('/', (_, res) => {
+    res.send(`app is running on PORT ${PORT}`);
+});
