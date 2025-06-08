adae6cc5d96feafebc85ac1dd1fafa1c6ae58c8f
Anita Kusumasari <anita.kusumasari@zettacamp.pro>
2025-06-09 03:20:50 +0700

    [ZettaCamp][Module 1][Project Setup & CRUD for Student and School Data] 1.2. Database Setup and Mongoose Model Definition: Update and Relocate the app.js

--- core/app.js
+++ core/app.js
@@ -2,9 +2,16 @@
 const express = require('express');
 const app = express()
 require('dotenv').config()
+const connectDB = require('./database');
+
+// *************** Get the MONGODB_URI value from environment
+const MONGODB_URI = process.env.MONGODB_URI;
 
 // *************** Setup PORT Value from Environment
 const PORT = process.env.PORT || 4000;
+
+// ****************** Calling the connectDB to connect the app with the MongoDB database
+connectDB(MONGODB_URI);
 
 // ****************** Starting the server on the PORT (4000)
 app.listen(PORT, () => {
