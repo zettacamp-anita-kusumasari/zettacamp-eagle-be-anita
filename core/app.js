a90e52b1966374d3ffe4d9b0e4a05cac29fc18f7
Anita Kusumasari <anita.kusumasari@zettacamp.pro>
2025-06-10 08:38:28 +0700

    [ZettaCamp][Module 1][Project Setup & CRUD for Student and School Data] 1.6. Implement CRUD Resolvers for School Model: Update app.js

--- core/app.js
+++ core/app.js
@@ -1,8 +1,15 @@
 // *************** IMPORT CORE ***************
 const express = require('express');
 const app = express();
-require('dotenv').config();
 const connectDB = require('./database');
+require('dotenv').config();
+const { ApolloServer } = require('apollo-server-express');
+const schoolTypeDefs = require('../modules/School/School.typeDefs.js');
+const studentTypeDefs = require('../modules/Student/Student.typeDefs.js');
+const userTypeDefs = require('../modules/User/User.typeDefs.js');
+const schoolResolvers = require('../modules/School/School.resolvers.js');
+const studentResolvers = require('../modules/Student/Student.resolvers.js');
+const userResolvers = require('../modules/User/User.resolvers.js');
 
 // *************** Get the MONGODB_URI value from environment
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -12,19 +19,6 @@ const PORT = process.env.PORT || 4000;
 
 // ****************** Calling the connectDB to connect the app with the MongoDB database
 connectDB(MONGODB_URI);
-
-// *************** Import Apollo Server middleware for Express
-const { ApolloServer } = require('apollo-server-express');
-
-// *************** Import GraphQL type definitions
-const schoolTypeDefs = require('../modules/School/School.typeDefs.js');
-const studentTypeDefs = require('../modules/Student/Student.typeDefs.js');
-const userTypeDefs = require('../modules/User/User.typeDefs.js');
-
-// *************** Import resolvers for type definitions
-const schoolResolvers = require('../modules/School/School.resolvers.js');
-const studentResolvers = require('../modules/Student/Student.resolvers.js');
-const userResolvers = require('../modules/User/User.resolvers.js');
 
 // *************** Async function to start Apollo Server and integrate it with Express app
 async function startApolloServer() {
