54c8396bd5371e2f4b0a2b242ef37e3850b662dd
Anita Kusumasari <anita.kusumasari@zettacamp.pro>
2025-06-17 02:14:14 +0700

    [ZettaCamp][Module 1][Project Setup & CRUD for Student and School Data] 1.7. Implement DataLoader for School-Student Relationship: Create typeDefs.js

---
+++ core/typeDefs.js
@@ -0,0 +1,17 @@
+// *************** IMPORT LIBRARY ***************
+const { gql } = require('apollo-server-express');
+
+// *************** IMPORT MODULE ***************
+const schoolTypeDefs = require('../modules/School/School.typeDefs.js');
+const studentTypeDefs = require('../modules/Student/Student.typeDefs.js');
+const userTypeDefs = require('../modules/User/User.typeDefs.js');
+
+// *************** Collects all GraphQL type definitions from School, Student, and User modules
+const TypeDefs = [
+  schoolTypeDefs,
+  studentTypeDefs,
+  userTypeDefs
+];
+
+// *************** EXPORT MODULE ***************
+module.exports = TypeDefs;
