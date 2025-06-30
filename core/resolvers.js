6009695445e5cdd6719d5bef2647b47bc99030da
Anita Kusumasari <anita.kusumasari@zettacamp.pro>
2025-06-30 18:11:26 +0700

    [ZettaCamp][Module 2][Test Management & Lifecycle]: Update resolvers.js

--- core/resolvers.js
+++ core/resolvers.js
@@ -2,6 +2,7 @@
 const SchoolResolvers = require('../modules/School/School.resolvers');
 const StudentResolvers = require('../modules/Student/Student.resolvers');
 const UserResolvers = require('../modules/User/User.resolvers');
+const BlockResolvers = require('../modules/Block/Block.resolvers');
 
 // *************** Combine all Query resolvers from School, Student, and User
 const Resolvers = {
@@ -10,8 +11,10 @@ const Resolvers = {
     ...SchoolResolvers.Query,
     // *************** Spread Student-related query resolvers (GetAllStudents and GetOneStudent)
     ...StudentResolvers.Query,
-    // *************** Spread User-related query resolvers (GetAllStudents and GetOneStudent)
-    ...UserResolvers.Query
+    // *************** Spread User-related query resolvers (GetAllUsers and GetOneUser)
+    ...UserResolvers.Query,
+    // *************** Spread Block-related query resolvers (GetAllBlocks and GetOneBlock)
+    ...BlockResolvers.Query
   },
   Mutation: {
     // *************** Spread School-related mutation resolvers (CreateSchool, UpdateSchool, and DeleteSchool)
@@ -19,7 +22,9 @@ const Resolvers = {
     // *************** Spread Student-related mutation resolvers (CreateStudent, UpdateStudent, and DeleteStudent)
     ...StudentResolvers.Mutation,
     // *************** Spread User-related mutation resolvers (CreateUser, UpdateUser, and DeleteUser)
-    ...UserResolvers.Mutation
+    ...UserResolvers.Mutation,
+    // *************** Spread Block-related mutation resolvers (CreateBlock, UpdateBlock, and DeleteBlock)
+    ...BlockResolvers.Mutation
   },
   // *************** Field-level resolvers for Student type (e.g., school, created_by)
   Student: StudentResolvers.Student,
