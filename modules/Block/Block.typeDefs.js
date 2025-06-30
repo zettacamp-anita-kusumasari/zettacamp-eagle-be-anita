90226af59a861cc6ebab3b4268c6f487a6b8957d
Anita Kusumasari <anita.kusumasari@zettacamp.pro>
2025-06-30 18:10:41 +0700

    [ZettaCamp][Module 2][Test Management & Lifecycle]: Create Block.typeDefs.js

---
+++ modules/Block/Block.typeDefs.js
@@ -0,0 +1,88 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+scalar Date
+
+enum BlockStatus {
+  ACTIVE
+  COMPLETED
+}
+
+enum BlockType {
+  REGULER
+  PROFESSIONAL
+  SOFT_SKILL
+  RETAKE
+}
+
+enum EvaluationAssessment {
+  COMPETENCY
+  SCORE
+}
+
+type User {
+  id: ID!
+  firstName: String!
+  lastName: String!
+  email: String!
+}
+
+type Subject {
+  id: ID!
+  name: String!
+  description: String
+  coefficient: Float!
+}
+
+type Block {
+  id: ID!
+  name: String!
+  description: String
+  subject_ids: [Subject]
+  school_id: ID
+  academic_year: Int!
+  block_code: String!
+  block_status: BlockStatus!
+  block_type: BlockType!
+  evaluation_assessment: EvaluationAssessment!
+  created_by: User
+  created_at: Date
+  updated_by: User
+  updated_at: Date
+  deleted_by: User
+  deleted_at: Date
+}
+
+input CreateBlockInput {
+  name: String!
+  description: String
+  academic_year: Int!
+  block_code: String!
+  block_status: BlockStatus!
+  block_type: BlockType!
+  evaluation_assessment: EvaluationAssessment!
+}
+
+input UpdateBlockInput {
+  name: String!
+  description: String
+  academic_year: Int!
+  block_code: String!
+  block_status: BlockStatus!
+  block_type: BlockType!
+  evaluation_assessment: EvaluationAssessment!
+}
+
+type Query {
+  GetAllBlocks: [Block!]!
+  GetOneBlock(id: ID!): Block
+}
+
+type Mutation {
+  CreateBlock(input: CreateBlockInput!): Block!
+  UpdateBlock(id: ID!, input: UpdateBlockInput!): Block!
+  DeleteBlock(id: ID!): Block!
+}
+`;
+
+module.exports = typeDefs;
