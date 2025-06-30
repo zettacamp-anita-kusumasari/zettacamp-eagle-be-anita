675e6df1e867357d8646136b4122e9b4ef2610ea
Anita Kusumasari <anita.kusumasari@zettacamp.pro>
2025-06-30 18:09:39 +0700

    [ZettaCamp][Module 2][Test Management & Lifecycle]: Create Block.loader.js

---
+++ modules/Block/Block.loader.js
@@ -0,0 +1,28 @@
+// *************** IMPORT LIBRARY ***************
+const DataLoader = require('dataloader');
+
+// *************** IMPORT MODULE ***************
+const BlockModel = require('./Block.model.js');
+
+async function BatchBlocks(ids) {
+  // *************** Fetch blocks matching the given IDs
+  const blocks = await BlockModel.find({ _id: { $in: ids } });
+  // *************** Map block ID to block object
+  const blockMap = new Map();
+  // *************** Store each block in the map with its ID as the key
+  for (const block of blocks) {
+    blockMap.set(String(block._id), block);
+  }
+  // *************** Return blocks in the same order as input IDs
+  const toOrderedBlocks = ids.map(id => blockMap.get(String(id)));
+  return toOrderedBlocks;
+}
+
+function CreateBlockLoader() {
+  // *************** Return DataLoader to BatchSchools
+  const toBlockLoader = new DataLoader(BatchBlocks);
+  return toBlockLoader;
+}
+
+// *************** EXPORT MODULE ***************
+module.exports = CreateBlockLoader;
