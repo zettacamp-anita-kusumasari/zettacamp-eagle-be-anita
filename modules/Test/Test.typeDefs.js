401a6bb97c0cd0b27e96062d3cb794f45c441d01
Anita Kusumasari <anita.kusumasari@zettacamp.pro>
2025-07-01 17:31:26 +0700

    [ZettaCamp][Module 2][Test Management & Lifecycle]: Create Test.typeDefs.js

---
+++ modules/Test/Test.typeDefs.js
@@ -0,0 +1,95 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    scalar Date
+
+    enum TestStatus {
+        TO_DO
+        FINISHED
+    }
+
+    enum Corrector {
+        CERTIFIER
+        PREPARATION_CENTER
+    }
+
+    type User {
+        id: ID!
+        firstName: String!
+        lastName: String!
+        email: String!
+    }
+
+    type Subject {
+        id: ID!
+        name: String!
+        description: String!
+        coefficient: Float!
+    }
+
+    type Notation {
+        notation_text: String!
+        max_point: Float!
+    }
+
+    type Test {
+        id: ID!
+        subject_id: Subject
+        name: String!
+        description: String!
+        weight: Float!
+        notations: [Notation!]!
+        test_status: TestStatus!
+        for_retake: Boolean!
+        corrector: Corrector!
+        published_by: User
+        published_date: Date
+        created_by: User
+        created_at: Date
+        updated_by: User 
+        updated_at: Date 
+        deleted_by: User 
+        deleted_at: Date
+    }
+
+    input NotationInput {
+        notation_text: String!
+        max_point: Float!
+    }
+
+    input CreateTestInput {
+        name: String!
+        description: String!
+        weight: Float!
+        notations: [NotationInput!]!
+        test_status: TestStatus!
+        for_retake: Boolean!
+        corrector: Corrector!
+        published_date: Date
+    }
+
+    input UpdateTestInput {
+        name: String
+        description: String
+        weight: Float
+        notations: [NotationInput!]
+        test_status: TestStatus!
+        for_retake: Boolean!
+        corrector: Corrector!
+        published_date: Date
+    }
+
+    type Query {
+        GetAllTests: [Test!]!
+        GetOneTest(id: ID!): Test
+    }
+
+    type Mutation {
+        CreateTest(input: CreateTestInput!): Test!
+        PublishTest(id: ID!): Test!
+        UpdateTest(id: ID!, input: UpdateTestInput!): Test!
+        DeleteTest(id: ID!): Test!
+    }
+`;
+
+module.exports = typeDefs;
