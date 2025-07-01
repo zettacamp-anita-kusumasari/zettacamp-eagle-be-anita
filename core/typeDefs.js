e44a64c9f4eab7df23e562a98ad778c2a5f7cf14
Anita Kusumasari <anita.kusumasari@zettacamp.pro>
2025-07-01 17:32:30 +0700

    [ZettaCamp][Module 2][Test Management & Lifecycle]: Update typeDefs.js

--- core/typeDefs.js
+++ core/typeDefs.js
@@ -4,14 +4,16 @@ const studentTypeDefs = require('../modules/Student/Student.typeDefs.js');
 const userTypeDefs = require('../modules/User/User.typeDefs.js');
 const blockTypeDefs = require('../modules/Block/Block.typeDefs.js');
 const subjectTypeDefs = require('../modules/Subject/Subject.typeDefs.js');
+const testTypeDefs = require('../modules/Test/Test.typeDefs.js');
 
-// *************** Collects all GraphQL type definitions from School, Student, User, Block, and Subject modules
+// *************** Collects all GraphQL type definitions from School, Student, User, Block, Subject, and Test modules
 const TypeDefs = [
   schoolTypeDefs,
   studentTypeDefs,
   userTypeDefs,
   blockTypeDefs,
-  subjectTypeDefs
+  subjectTypeDefs,
+  testTypeDefs
 ];
 
 // *************** EXPORT MODULE ***************
