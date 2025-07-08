1aa7aa2a1e5d9f19d25cd0884bc6039a7d3bf47b
Anita Kusumasari <anita.kusumasari@zettacamp.pro>
2025-07-08 11:41:16 +0700

    [ZettaCamp][Module 2][Test Management & Lifecycle]: Update Block Module

--- modules/Block/Block.loader.js
+++ modules/Block/Block.loader.js
@@ -4,6 +4,15 @@ const DataLoader = require("dataloader");
 // *************** IMPORT MODULE ***************
 const BlockModel = require("./Block.model.js");
 
+/**
+ * Batch fetch Block documents by an array of IDs and return them in the same order as the input IDs.
+ *
+ * @async
+ * @function
+ * @param {Array<string|import('mongoose').Types.ObjectId>} ids - Array of Block document IDs.
+ * @returns {Promise<Array<Object|null>>} A Promise that resolves to an array of Block objects in the same order as `ids`.
+ *                                       If a block ID does not exist, `null` will be returned in its place.
+ */
 async function BatchBlocks(ids) { 
   // *************** Fetch blocks matching the given IDs
   const blocks = await BlockModel.find({ _id: { $in: ids } }).lean();
@@ -18,6 +27,12 @@ async function BatchBlocks(ids) {
   return toOrderedBlocks;
 }
 
+/**
+ * Creates a DataLoader instance for batching and caching Block document retrieval.
+ *
+ * @function
+ * @returns {import('dataloader')} A DataLoader instance for loading Blocks by ID.
+ */
 function CreateBlockLoader() {
   // *************** Return DataLoader to BatchSchools
   const toBlockLoader = new DataLoader(BatchBlocks);
